4b03e4a222df3691c10a230ae8974ee66e351442
its-sky <smc9919@naver.com>
2024-09-20 17:42:26 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/kotlin/distribution/lock/LockApplication.kt
@@ -0,0 +1,11 @@
+package distribution.lock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LockApplication
+
+fun main(args: Array<String>) {
+	runApplication<LockApplication>(*args)
+}
